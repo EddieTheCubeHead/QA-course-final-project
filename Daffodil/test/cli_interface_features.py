f882125e7739aab1560d3a0a189bb0f7fc90dc2e
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2023-12-06 09:02:38 +0200

    Remove example tests, add basic error case

--- Daffodil/test/cli_interface_features.py
+++ Daffodil/test/cli_interface_features.py
@@ -1,10 +1,11 @@
-import subprocess
-
-import pytest
-
 from conftest import cli_runner_wrapper
 
 
 def should_print_version_when_version_argument_supplied(cli_runner: cli_runner_wrapper):
     result = cli_runner("--version")
     assert result.stdout == "Apache Daffodil 3.6.0"
+
+
+def should_print_error_if_ran_without_args(cli_runner: cli_runner_wrapper):
+    result = cli_runner()
+    assert result.stderr == "[error] Subcommand required"
--- Daffodil/test/example_features.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-# noinspection PyMethodMayBeStatic
-class ExampleFeatures:
-
-    def should_accept_true_being_true(self):
-        assert True
