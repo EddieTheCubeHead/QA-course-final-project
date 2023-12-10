3f404ed60ae9a9922159bc279c047899969bb28d
Leevi <joona.mlin@gmail.com>
2023-12-10 15:00:20 +0200

    Perf tests mostly done, missing timeout

--- Daffodil/test/performance_features.py
+++ Daffodil/test/performance_features.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from datetime import timedelta
 
@@ -10,16 +12,25 @@ def should_convert_json_performantly(cli_runner, generate_test_file: None, schem
     data_file_path = data_files["basic.csv"]
     with timer:
         result = cli_runner('parse', f'--schema', csv_schema, data_file_path, '-I', 'json').stdout
-    assert timer < date
-    file_data = File.model_validate_json(result)
+    assert timer < timedelta(seconds=60)
 
 @pytest.mark.parametrize("generate_test_file", (("en_US")), indirect=True)
-def should_convert_xml_performantly(cli_runner, generate_test_file: None):
-    pass
+def should_convert_xml_performantly(cli_runner, generate_test_file: None, data_files, schemas):
+    csv_schema = schemas["csv"]
+    data_file_path = data_files["basic.csv"]
+    with timer:
+        result = cli_runner('parse', f'--schema', csv_schema, data_file_path, '-I', 'xml').stdout
+    assert timer < timedelta(seconds=60)
 
 @pytest.mark.parametrize("generate_test_file", (("en_US")), indirect=True)
-def should_unparse_json_performantly(cli_runner, generate_test_file: None):
-    pass
+def should_unparse_json_performantly(cli_runner, generate_test_file: None, schemas, data_files, data_output_directory, ):
+    csv_schema = schemas["csv"]
+    data_file_path = data_files["basic.csv"]
+    xml_output_path = os.path.join(data_output_directory, "test.xml")
+    csv_output_path = os.path.join(data_output_directory, "test.csv")
+    with timer:
+        cli_runner('unparse', f'--schema', csv_schema, xml_output_path, '-o', csv_output_path, '-I', 'xml')
+    
 
 @pytest.mark.parametrize("generate_test_file", (("en_US")), indirect=True)
 def should_unparse_xml_performantly(cli_runner, generate_test_file: None):
